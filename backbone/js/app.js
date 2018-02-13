c7d9de15a3b19af69899828587dfad376aa2c55e
UCDM <ino.winter.1212@gmail.com>
2018-02-13 20:09:11 +0900

    2/13ここまで

---
+++ backbone/js/app.js
@@ -0,0 +1,16 @@
+(function(){
+
+var Task = Backbone.Model.extend({
+  defaults: {
+    title: "do something!",
+    completed: false
+  }
+});
+
+var task1 = new Task({
+  completed: true
+});
+
+console.log(task1.toJSON());
+
+})();
