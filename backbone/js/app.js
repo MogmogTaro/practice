fd4c31d1990efe17ffaee9cda19d2b87b266db41
UCDM <ino.winter.1212@gmail.com>
2018-02-15 11:22:07 +0900

    2/14までの分

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -4,6 +4,14 @@ var Task = Backbone.Model.extend({
   defaults: {
     title: "do something!",
     completed: false
+  },
+  validate: function(attrs) {
+    if ( _ . isEmpty(attrs.title)) {
+      return "title must not be empty!";
+    }
+  },
+  toggle: function() {
+    this.set('completed' , !this.get('completed'));
   }
 });
 
@@ -11,6 +19,13 @@ var task1 = new Task({
   completed: true
 });
 
-console.log(task1.toJSON());
+// task1.set('title' , 'newTitle');
+// var title = task1.get('title');
+//
+// console.log(title);
 
+console.log(task1.toJSON());
+task1.set({title : ''} , { validate:true });
+//task1.toggle();
+console.log(task1.toJSON());
 })();
