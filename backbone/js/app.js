ddaeddcb60c92259e70d1764d576c7f0b35b3350
UCDM <ino.winter.1212@gmail.com>
2018-02-21 19:19:35 +0900

    ＃12 ToDoアプリを作っていこう

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -1,45 +1,10 @@
 (function(){
 
-// Model
+var Task = backbone.Model.extend({});
+var Tasks = Backbone.Collection.extend({});
 
-var Task = Backbone.Model.extend({
-  defaults: {
-    title: "do something!",
-    completed: false
-  }
-});
-
-var task = new Task();
-
-// view
-
-var TaskView = Backbone.View.extend({
-  tagName: 'li',
-  template: _.template( $('#task-template').html()),
-  render: function() {
-    var template = this.template(this.model.toJSON());
-    this.$el.html(template);
-    return this;
-  }
-});
-
-//collection
-
-var Tasks = Backbone.Collection.extend({
-  model: Task
-});
-
-var TasksView = Backbone.View.extend({
-  tagName: 'ul',
-  render: function() {
-    this.collection.each(function(task){
-      var taskView = new TaskView({model:task});
-      this.$el.append(taskView.render().el);
-    }, this);
-    return this;
-  }
-});
-
+var TaskView = Backbone.View.extend({});
+var TasksView = Backbone.View.extend({});
 
 var tasks = new Tasks([
   {
@@ -47,16 +12,11 @@ var tasks = new Tasks([
     completed: true
   },
   {
-    title:'task2'
+    title:'task2',
   },
   {
-    title:'task3'
+    title:'task3',
   }
 ]);
-
-//console.log(tasks.toJSON());
-
-var tasksView = new TasksView({collection:tasks});
-$('#tasks').html(tasksView.render().el);
 
 })();
