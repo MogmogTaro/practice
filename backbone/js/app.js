f2f41171a6707c82b1eb0ff79d67c0eeae3ce0fb
UCDM <ino.winter.1212@gmail.com>
2018-02-21 18:52:07 +0900

    ドットインストール8

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -17,7 +17,7 @@ var TaskView = Backbone.View.extend({
   tagName: 'li',
   //className: 'liClass',
   //id: 'liid'
-  template: _.template("<%- title %>"),
+  template: _.template( $('#task-template').html()),
   render: function() {
     var template = this.template(this.model.toJSON());
     this.$el.html(template);
@@ -26,6 +26,7 @@ var TaskView = Backbone.View.extend({
 });
 var taskView = new TaskView({model : task});
 console.log(taskView.render().el)
+$(body).append(taskView.render().el);
 // console.log(taskView.$el)
 //$でjqueryのメソッドになる
 })();
