4c994af8a4d53b005f1ef76542af911353b6f538
UCDM <ino.winter.1212@gmail.com>
2018-02-21 19:10:10 +0900

    ＃11 CollectionのViewを作ってみよう

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -28,6 +28,19 @@ var TaskView = Backbone.View.extend({
 var Tasks = Backbone.Collection.extend({
   model: Task
 });
+
+var TasksView = Backbone.View.extend({
+  tagName: 'ul',
+  render: function() {
+    this.collection.each(function(task){
+      var taskView = new TaskView({model:task});
+      this.$el.append(taskView.render().el);
+    }, this);
+    return this;
+  }
+});
+
+
 var tasks = new Tasks([
   {
     title:'task1',
@@ -41,6 +54,9 @@ var tasks = new Tasks([
   }
 ]);
 
-console.log(tasks.toJSON());
+//console.log(tasks.toJSON());
+
+var tasksView = new TasksView({collection:tasks});
+$('#tasks').html(tasksView.render().el);
 
 })();
