24aaaad35646a3afdf9ca7905bb4651984587942
UCDM <ino.winter.1212@gmail.com>
2018-02-22 19:26:57 +0900

    ＃14 tasksのViewを作っていこう

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -1,6 +1,6 @@
-(function(){
+(function() {
 
-var Task = backbone.Model.extend({
+var Task = Backbone.Model.extend({
   defaults: {
     title: 'do something!',
     completed: false
@@ -10,14 +10,23 @@ var Tasks = Backbone.Collection.extend({model:Task});
 
 var TaskView = Backbone.View.extend({
   tagName: 'li',
-  template: _.template($('#task-template').html());
+  template: _.template($('#task-template').html()),
   render: function(){
     var template = this.template(this.model.toJSON());
     this.$el.html(template);
     return this;
   }
 });
-var TasksView = Backbone.View.extend({});
+var TasksView = Backbone.View.extend({
+  tagName: 'ul',
+  render: function(){
+    this.collection.each(function(task) {
+      var taskView = new TaskView({model:task});
+      this.$el.append(taskView.render().el);
+    },this);
+    return this;
+  }
+});
 
 var tasks = new Tasks([
   {
@@ -31,5 +40,8 @@ var tasks = new Tasks([
     title:'task3',
   }
 ]);
+
+var tasksView = new TasksView({collection: tasks});
+$('#tasks').html(tasksView.render().el);
 
 })();
